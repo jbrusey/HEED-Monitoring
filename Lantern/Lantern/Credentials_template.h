d371fb103620510d28a606932c62a65b639152f7
Gabriel Arnautu <arnautug7@gmail.com>
2018-10-31 17:14:45 +0000

    Add Credentials.h template

---
+++ Lantern/Lantern/Credentials_template.h
@@ -0,0 +1,10 @@
+//CREDENTIALS - SIM
+#define SECRET_PINNUMBER     ""               // SIM Pin number
+#define SECRET_GPRS_APN      "giffgaff.com"               // replace GPRS APN
+#define SECRET_GPRS_LOGIN    "giffgaff"               // replace with GPRS login
+#define SECRET_GPRS_PASSWORD ""               // replace with GPRS password
+
+//CREDENTIALS - MQTT
+#define MQTT_SERVER "broker.hivemq.com"                        // MQTT broker address
+#define MQTT_USER ""
+#define MQTT_PASS ""
